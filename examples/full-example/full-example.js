6d5e2a19f9673c6f958e3f5a6c40c35c620457a9
PioArchiver <46547840+PioArchiver@users.noreply.github.com>
2019-07-24 01:16:23 -0600

    Update full-example.js

--- examples/full-example/full-example.js
+++ examples/full-example/full-example.js
@@ -7,12 +7,12 @@ let Dashing = null;
 window.addEventListener("load", function LoadExample() {
     Dashing = document.querySelector("x-extension");
 
-    Dashing.queryJson("full-example", Dashing.jsonSchema, "#").then(function SchemaDemoQuery(val) {
+let s = Dashing.queryJson("full-example", Dashing.jsonSchema, "#").then(function SchemaDemoQuery(val) {
         console.log(val);
         return val;
     }).catch(function CatchSchemaDemoQuery(val) { console.log(val); });
 
 });
-
+    console.log(s);
 
 })();
