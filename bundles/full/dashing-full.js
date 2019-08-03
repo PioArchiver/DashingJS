a35012e4f2bcf11b696cfe033f65ef14943c33ca
PioArchiver <46547840+PioArchiver@users.noreply.github.com>
2019-08-03 14:43:54 -0600

    Update dashing-full.js

--- bundles/full/dashing-full.js
+++ bundles/full/dashing-full.js
@@ -1659,7 +1659,7 @@ ____________________ **/
                         },
                         addResizerIcons: function AddResizerIcons(icons, opts) {
                             for (let i = 0; i < icons.length; i++) {
-                                console.log(`div[panel-resizer] > button[icon="${icons[i]}]"`);
+                                console.log(`div[panel-resizer] > button[icon="${icons[i]}"]`);
                                 this.extension.icons.append(this.xMenu.querySelector(`div[panel-resizer] > button[icon="${icons[i]}"]`),
                                     { icon: icons[i] });
                             } 
