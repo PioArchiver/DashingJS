dabc4d835e1ada6431830250a0dca5ecf06f6bab
PioArchiver <46547840+PioArchiver@users.noreply.github.com>
2019-08-06 04:14:49 -0600

    Update dashing-full.js

--- bundles/full/dashing-full.js
+++ bundles/full/dashing-full.js
@@ -296,7 +296,7 @@ ____________________ **/
         set drawer(value) { this.draw = Dashing.typeOf(value) === "function" ? value : false; }
         get drawer() { return this.draw; }
         set uploader(value) {
-            console.log(Dashing.requests);
+            console.log(Dashing.model.requests);
             Dashing.typeOf(value) === "object" ? (Dashing.model.requests[value.id] ? value.uploader ? value.uploader(Dashing.model.requests[value.id]) : false : this.Uploader = value.uploader) : function UndefinedError() { return "Caution: No uploader available."; }; 
         }
         get uploader() { return this.Uploader; }
